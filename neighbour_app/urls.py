39e50fabf16262fc3cd27206a915dae3bd915629
otienonick <nick.odongi@student.moringaschool.com>
2021-10-28 18:49:19 +0300

    create urls module and home path

---
+++ neighbour_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path 
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import home
+
+urlpatterns = [
+    path('' , home , name='home'),
+
+
+
+
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
