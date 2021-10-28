e4c5f08ab0fa8a4239fe0ab5a645715e9e230298
otienonick <nick.odongi@student.moringaschool.com>
2021-10-28 17:02:50 +0300

    create app project

--- neighbour_project/settings.py
+++ neighbour_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=3$t76#rci9_x$8rw-$oj2fxc5hgeu!!i+38-9wh05aw!nsz03'
+SECRET_KEY = 'django-insecure-xp=lz743k7ozfbx6r4$ff_wkyi9z!6d2fx1d(=i3de08-v$&$*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
