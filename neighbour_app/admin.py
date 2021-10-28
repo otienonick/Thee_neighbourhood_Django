58eb0cbcff202d8175c9446155c9948388d4523b
otienonick <nick.odongi@student.moringaschool.com>
2021-10-28 22:30:17 +0300

    added Post model

--- neighbour_app/admin.py
+++ neighbour_app/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import Profile,Neighbourhood,Business
+from .models import Profile,Neighbourhood,Business,Post
 
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Neighbourhood)
 admin.site.register(Business)
-
-
+admin.site.register(Post)
