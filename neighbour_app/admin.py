ffd6981330abe3ceb1c5a6b836663bbe89b759e8
otienonick <nick.odongi@student.moringaschool.com>
2021-10-28 20:21:10 +0300

    add business model

--- neighbour_app/admin.py
+++ neighbour_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Profile,Neighbourhood
+from .models import Profile,Neighbourhood,Business
 
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Neighbourhood)
+admin.site.register(Business)
+
 
