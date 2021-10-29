f757d8ad092145e3eb91a0825d6343299f32c751
otienonick <nick.odongi@student.moringaschool.com>
2021-10-29 15:45:45 +0300

    create homepage path

--- neighbour_app/urls.py
+++ neighbour_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path 
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import home,my_profile_view
+from .views import home,my_profile_view,home_page
 
 urlpatterns = [
     path('' , home , name = 'home'),
     path('myprofile/',my_profile_view,name ='profile'),
+    path('homepage/',home_page,name ='homepage'),
+
 
 
 
