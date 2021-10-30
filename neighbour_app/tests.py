58dc190ef569aeec9ef324e58cf47aca814b93cf
otienonick <nick.odongi@student.moringaschool.com>
2021-10-30 14:20:34 +0300

    update and run 15 sucessful tests

--- neighbour_app/tests.py
+++ neighbour_app/tests.py
@@ -7,96 +7,46 @@ from django.contrib.auth.models import User
 class ProfileTestClass(TestCase):
     def setUp(self):
         user = User.objects.create()
-        self.oti = Profile(username = 'test_username',user = user, email = 'test_email',identity = '123456',created = '28-10-2021',updated = '28-10-2021' )
+        self.oti = Profile(username = 'test_username',user = user, email = 'test_email',identity = '123456',created = '28-10-2021',updated = '28-10-2021' ,hood = 'test_hood',location = 'test_location',occupants = '5000')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.oti,Profile))    
 
     def test_save_method(self):
-        self.oti.save_profile()
-        editors = Profile.objects.all()
-        self.assertTrue(len(editors) > 0)   
-      
-# class NeighbourhoodTestClass(TestCase):
-#     def setUp(self):
-#         user = User.objects.create()
-        
-#         self.new_neighbourhood = Neighbourhood(name = 'test_name',location = 'test_location',occupants = '5000',admin = user)
+        self.oti.create_neighbourhood()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) > 0)   
 
-#     def test_instance(self):
-#             self.assertTrue(isinstance(self.new_neighbourhood,Neighbourhood)) 
+    def tearDown(self):
+        Profile.objects.all().delete()
 
-#     def test_save_method(self):
-#             self.new_neighbourhood.create_neighbourhood()
-#             neighbourhood = Neighbourhood.objects.all()
-#             self.assertTrue(len(neighbourhood) > 0)   
+    def test_delete_method(self):
+        self.oti.create_neighbourhood()
+        self.oti.delete_neighbourhood()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) == 0)     
 
-#     def tearDown(self):
-#             Profile.objects.all().delete()
-#             Neighbourhood.objects.all().delete()
+    def test_update_method(self):
+        self.oti.update_neighbourhood()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) > 0) 
 
-#     def test_delete_method(self):
-#             self.new_neighbourhood.create_neighbourhood()
-#             self.new_neighbourhood.delete_neighbourhood()class NeighbourhoodTestClass(TestCase):
-#     def setUp(self):
-#         user = User.objects.create()
-        
-#         self.new_neighbourhood = Neighbourhood(name = 'test_name',location = 'test_location',occupants = '5000',admin = user)
+    def test_update_occupants__method(self):
+        self.oti.update_occupants()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) > 0)    
 
-#     def test_instance(self):
-#             self.assertTrue(isinstance(self.new_neighbourhood,Neighbourhood)) 
-
-#     def test_save_method(self):
-#             self.new_neighbourhood.create_neighbourhood()
-#             neighbourhood = Neighbourhood.objects.all()
-#             self.assertTrue(len(neighbourhood) > 0)   
-
-#     def tearDown(self):
-#             Profile.objects.all().delete()
-#             Neighbourhood.objects.all().delete()
-
-#     def test_delete_method(self):
-#             self.new_neighbourhood.create_neighbourhood()
-#             self.new_neighbourhood.delete_neighbourhood()
-#             neighbourhood = Neighbourhood.objects.all()
-#             self.assertTrue(len(neighbourhood) == 0)     
-
-#     def test_update_method(self):
-#             self.new_neighbourhood.update_neighbourhood()
-#             neighbourhood = Neighbourhood.objects.all()
-#             self.assertTrue(len(neighbourhood) > 0) 
-
-#     def test_update_occupants__method(self):
-#             self.new_neighbourhood.update_occupants()
-#             neighbourhood = Neighbourhood.objects.all()
-#             self.assertTrue(len(neighbourhood) > 0)    
-
-#     def test_find_neighbourhood(self):
-#         neighbourhood_id = Neighbourhood.find_neighbourhood()
-#         self.assertTrue(len(neighbourhood_id) == 0)    
-
-#             neighbourhood = Neighbourhood.objects.all()
-#             self.assertTrue(len(neighbourhood) == 0)     
-
-#     def test_update_method(self):
-#             self.new_neighbourhood.update_neighbourhood()
-#             neighbourhood = Neighbourhood.objects.all()
-#             self.assertTrue(len(neighbourhood) > 0) 
-
-#     def test_update_occupants__method(self):
-#             self.new_neighbourhood.update_occupants()
-#             neighbourhood = Neighbourhood.objects.all()
-#             self.assertTrue(len(neighbourhood) > 0)    
-
-#     def test_find_neighbourhood(self):
-#         neighbourhood_id = Neighbourhood.find_neighbourhood()
-#         self.assertTrue(len(neighbourhood_id) == 0)    
+    def test_find_neighbourhood(self):
+        neighbourhood_id = Profile.find_neighbourhood()
+        self.assertTrue(len(neighbourhood_id) == 0)    
+  
 
 class BusinessTestClass(TestCase):
 
     def setUp(self):
         user = User.objects.create()
-        self.oti = Profile(username = 'test_username',user = user, email = 'test_email',identity = '123456',created = '28-10-2021',updated = '28-10-2021' )
+        self.oti = Profile(username = 'test_username',user = user, email = 'test_email',identity = '123456',created = '28-10-2021',updated = '28-10-2021' ,hood = 'test_hood',location = 'test_location',occupants = '5000')
+        self.oti.save()
 
         self.new_business = Business(name = 'test_business_name',user = user,neighbourhood_id = self.oti,email = 'test_email')
         self.new_business.create_business()
